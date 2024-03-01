050281b68a1eddeca1149bf6e342dd036e18198e
Ignacio Quirelli Pressiani <Ignacio.Quirelli@tunstall.com>
2024-03-01 18:12:30 +0100

    refactor: splitting code into methods

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,89 +14,36 @@ namespace ConsoleUI
             string w, rawTimeWorked;
             int i;
             double ttl, t;
-            List<TimeSheetEntry> ents = new List<TimeSheetEntry>();
-            Console.Write("Enter what you did: ");
-            w = Console.ReadLine();
-            Console.Write("How long did you do it for: ");
-            rawTimeWorked = Console.ReadLine();
 
-            while (double.TryParse(rawTimeWorked, out t) == false)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Invalid number given");
-                Console.Write("How long did you do it for: ");
-                rawTimeWorked = Console.ReadLine();
-            }
+            List<TimeSheetEntry> timeSheets = LoadTimeSheets();
 
-            TimeSheetEntry ent = new TimeSheetEntry();
-            ent.HoursWorked = t;
-            ent.WorkDone = w;
-            ents.Add(ent);
-            Console.Write("Do you want to enter more time (yes/no): ");
-
-            string answer = Console.ReadLine();
-            bool cont = false;
-
-            if (answer.ToLower() == "yes")
-            {
-                cont = true;
-            }
-
-            while (cont == true)
-            {
-                Console.Write("Enter what you did: ");
-                w = Console.ReadLine();
-                Console.Write("How long did you do it for: ");
-                rawTimeWorked = Console.ReadLine();
-
-                while (double.TryParse(rawTimeWorked, out t) == false)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Invalid number given");
-                    Console.Write("How long did you do it for: ");
-                    rawTimeWorked = Console.ReadLine();
-                }
-
-                ent = new TimeSheetEntry();
-                ent.HoursWorked = t;
-                ent.WorkDone = w;
-                ents.Add(ent);
-
-                Console.Write("Do you want to enter more time (yes/no): ");
-                answer = Console.ReadLine();
-                cont = false;
-
-                if (answer.ToLower() == "yes")
-                {
-                    cont = true;
-                }
-            } 
             ttl = 0;
-            for (i = 0; i < ents.Count; i++)
+
+            for (i = 0; i < timeSheets.Count; i++)
             {
-                if (ents[i].WorkDone.ToLower().Contains("acme"))
+                if (timeSheets[i].WorkDone.ToLower().Contains("acme"))
                 {
-                    ttl += ents[i].HoursWorked;
+                    ttl += timeSheets[i].HoursWorked;
                 }
             }
             Console.WriteLine("Simulating Sending email to Acme");
             Console.WriteLine("Your bill is $" + ttl * 150 + " for the hours worked.");
 
             ttl = 0;
-            for (i = 0; i < ents.Count; i++)
+            for (i = 0; i < timeSheets.Count; i++)
             {
-                if (ents[i].WorkDone.ToLower().Contains("abc"))
+                if (timeSheets[i].WorkDone.ToLower().Contains("abc"))
                 {
-                    ttl += ents[i].HoursWorked;
+                    ttl += timeSheets[i].HoursWorked;
                 }
             }
             Console.WriteLine("Simulating Sending email to ABC");
             Console.WriteLine("Your bill is $" + ttl * 125 + " for the hours worked.");
 
             ttl = 0;
-            for (i = 0; i < ents.Count; i++)
+            for (i = 0; i < timeSheets.Count; i++)
             {
-                ttl += ents[i].HoursWorked;
+                ttl += timeSheets[i].HoursWorked;
             }
             if (ttl > 40)
             {
@@ -110,11 +57,48 @@ namespace ConsoleUI
             Console.Write("Press any key to exit application...");
             Console.ReadKey();
         }
-    }
 
+        private static List<TimeSheetEntry> LoadTimeSheets()
+        {
+            List<TimeSheetEntry> output = new List<TimeSheetEntry>();
+            string enterMoreTimeSheets;
+
+            do
+            {
+                Console.Write("Enter what you did: ");
+                string workDone = Console.ReadLine();
+                Console.Write("How long did you do it for: ");
+                string rawTimeWorked = Console.ReadLine();
+
+                double hoursWorked;
+
+                while (double.TryParse(rawTimeWorked, out hoursWorked) == false)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid number given");
+                    Console.Write("How long did you do it for: ");
+                    rawTimeWorked = Console.ReadLine();
+                }
+
+                TimeSheetEntry timeSheet = new TimeSheetEntry();
+                timeSheet.HoursWorked = hoursWorked;
+                timeSheet.WorkDone = workDone;
+                output.Add(timeSheet);
+
+                Console.Write("Do you want to enter more time (yes/no): ");
+
+                enterMoreTimeSheets = Console.ReadLine();
+
+            } while (enterMoreTimeSheets.ToLower() == "yes");
+
+            return output;
+        }
+    }
     public class TimeSheetEntry
     {
         public string WorkDone;
         public double HoursWorked;
     }
+
+    
 }
